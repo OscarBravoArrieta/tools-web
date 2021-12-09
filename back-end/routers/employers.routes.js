bcf6a9efe595e46d3751ee18a91b94f66e71051a
oscarbravo <oscarbravoarrieta@gmail.com>
2021-12-09 17:06:43 -0500

    sendingHeaderToServer

--- back-end/routers/employers.routes.js
+++ back-end/routers/employers.routes.js
@@ -1,9 +1,10 @@
  import { Router } from 'express'
  const router = Router() 
  import * as employersCtrl from '../controllers/employers.controller'
+ import * as authjwt from '../middlewares/authjwt'
  
  router.put('/', employersCtrl.getAll)
- router.put('/getEmployeesEmployer', employersCtrl.getEmployeesEmployer)
+ router.put('/getEmployeesEmployer',  [authjwt.verifyToken], employersCtrl.getEmployeesEmployer)
  router.put('/getBeneficiariesEmployer', employersCtrl.getBeneficiariesEmployer)
  router.put('/getPaymentsEmployer', employersCtrl.getPaymentsEmployer)
  router.put('/getPayrollEmployer', employersCtrl.getPayrollEmployer)
